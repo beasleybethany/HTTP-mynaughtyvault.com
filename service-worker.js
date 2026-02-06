2e75acea8f0329b443a05f12dd853e696a22432a
Bethany Beasley <beasleybethany6@Gmail.com>
2026-02-05 19:49:33 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,17 @@
-self.addEventListener('install',()=>self.skipWaiting());
+const CACHE_NAME = 'bethany-luxe-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/js/protect.js',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
